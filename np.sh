16d721c3a48f4bcf93e2757b3b1130a1cf2158e6
Marcus Carlsson <mc@beepsend.se>
2013-03-04 10:03:55 +0100

    Use dbus-send instead of qdbus.

--- np.sh
+++ np.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-spotify=$(ps -C spotify | wc -l)
 tomahawk=$(ps -C tomahawk | wc -l)
 
 if [ $tomahawk -gt 1 ]; then
-    playing=$(qdbus org.mpris.MediaPlayer2.tomahawk /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus)
+    playing=$(dbus-send --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.tomahawk /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:"org.mpris.MediaPlayer2.Player" string:"PlaybackStatus" | sed -n '/variant/ s/^.*"\([^"]\+\).*$/\1/p')
     if [ $playing == 'Playing' ]; then
-        data="np: $(qdbus org.mpris.MediaPlayer2.tomahawk /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Metadata | sed -n '/artist/{ N; s/.*: \(.*\)\n.*: \(.*\)$/\1 - \2/p; }')"
+        data="np: $(dbus-send --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.tomahawk /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:"org.mpris.MediaPlayer2.Player" string:"Metadata" | sed -n '/artist/{ n; n; h; };/title/{ n; H; x; s/^.*"\([^"]\+\)"\n.*"\([^"]\+\)".*$/\1 - \2/p}')"
         echo $data
     fi
 fi
