7e154d7822154cee5c03e50e2fbec219fbf27e8c
Marcus Carlsson <mc@beepsend.se>
2013-05-24 14:04:32 +0200

    np: Now handles both tomahawk and spotify.

--- np.sh
+++ np.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 tomahawk=$(ps -C tomahawk | wc -l)
+spotify=$(ps -C spotify | wc -l)
 
 if [ $tomahawk -gt 1 ]; then
     playing=$(dbus-send --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.tomahawk /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:"org.mpris.MediaPlayer2.Player" string:"PlaybackStatus" | sed -n '/variant/ s/^.*"\([^"]\+\).*$/\1/p')
     if [ $playing == 'Playing' ]; then
         data="np: $(dbus-send --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.tomahawk /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:"org.mpris.MediaPlayer2.Player" string:"Metadata" | sed -n '/artist/{ n; n; h; };/title/{ n; H; x; s/^.*"\([^"]\+\)"\n.*"\([^"]\+\)".*$/\1 - \2/p}')"
-        echo $data
+        if [ $# -gt 0 ] && [ $1 = 'space' ]; then
+            echo " $data "
+        else
+            echo $data
+        fi
+    fi
+fi
+
+if [ $spotify -gt 1 ]; then
+    playing=$(dbus-send --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:"org.mpris.MediaPlayer2.Player" string:"PlaybackStatus" | sed -n '/variant/s/^.*"\([^"]\+\).*$/\1/p')
+    if [ $playing == 'Playing' ]; then
+        data="np: $(dbus-send --type=method_call --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:"org.mpris.MediaPlayer2.Player" string:"Metadata" | sed -n '/artist/ { n;n; h}; /title/ { n; H; g; s/^.*"\([^"]\+\).*\n.*"\([^"]\+\).*$/\1 - \2/p };')"
+        if [ $# -gt 0 ] && [ $1 = 'space' ]; then
+            echo " $data "
+        else
+            echo $data
+        fi
     fi
 fi
